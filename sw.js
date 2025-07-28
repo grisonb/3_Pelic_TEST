ee0d6827237fe537468e79e76727db0c645889fd
grisonb <79510016+grisonb@users.noreply.github.com>
2025-07-29 00:27:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 // --- NOUVEAU CONTENU COMPLET ET CORRIGÉ POUR sw.js ---
 
 // MODIFICATION ICI 👇
-const APP_CACHE_NAME = 'communes-app-cache-v27'; // Version incrémentée
+const APP_CACHE_NAME = 'communes-app-cache-v28'; // Version incrémentée
 const TILE_CACHE_NAME = 'communes-tile-cache-v9';
-const DATA_CACHE_NAME = 'communes-data-cache-v5'; // Pas besoin de changer, les données n'ont pas changé
+const DATA_CACHE_NAME = 'communes-data-cache-v6'; // Pas besoin de changer, les données n'ont pas changé
 
 // On utilise des chemins relatifs pour que ça fonctionne partout
 const APP_SHELL_URLS = [
