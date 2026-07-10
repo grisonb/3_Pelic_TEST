7a64e72c2442c1eb8842db1ce436397a8ad4d96b
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-10 07:35:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-01-commune-gauche-trafic-filtres';
+const SW_VERSION = 'sw-v13-02-trafic-modal-commune-decalee';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
