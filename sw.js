4f14718111f4ba2329a2b5eaf6f9d3a4692c7686
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-10 16:11:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-18-fuel-depart-kg-meme-taille';
+const SW_VERSION = 'sw-v13-19-fuel-depart-kg-pleine-taille';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
