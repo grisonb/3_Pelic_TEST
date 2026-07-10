41ab4a90b37bdd98df1414428242580e8f4664ad
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-10 08:16:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-03-trafic-bouton-altitude-refresh';
+const SW_VERSION = 'sw-v13-04-refresh-5s-ligne-position-visible';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
