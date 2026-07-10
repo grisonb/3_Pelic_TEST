a02e2ba1fffcfd80fdfc04ca069b44bf13a3ab90
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-10 15:20:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-15-export-horodatage-filtre-vols';
+const SW_VERSION = 'sw-v13-16-export-nom-pdf-fuel-depart';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
