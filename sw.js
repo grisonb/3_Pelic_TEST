52a4a8c659b6fda89042a5bc1ff5351c270943d6
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-10 15:06:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-14-ui-export-sans-trafic';
+const SW_VERSION = 'sw-v13-15-export-horodatage-filtre-vols';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
