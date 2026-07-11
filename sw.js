84e8fe4f2178343d1ba713f669eb691702817569
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-11 19:09:12 +0200

    Update service worker version for PDF export

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-22-export-pdf-layout-propre';
+const SW_VERSION = 'sw-v13-23-export-pdf-colonnes-homogenes';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
