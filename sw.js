165246b9944e5acad5d41c40fb112e8ef0422a32
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-11 20:54:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-24-grand-clavier-recherche-feu';
+const SW_VERSION = 'sw-v13-25-maj-double-relance-clavier-large';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
