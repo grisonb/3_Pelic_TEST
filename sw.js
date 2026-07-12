3a5501aacfc2cc40bd02e9e0d79a30d7394cc960
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-12 09:10:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-27-message-maj-suivi-centrage-gps';
+const SW_VERSION = 'sw-v13-28-suivi-gps-drag-zip-ign';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
