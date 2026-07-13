a6fba93318de4cf1065d9df1f11c3aba0528d34c
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-13 15:17:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-38-historique-suppression-un-clic-fenetre-ouverte';
+const SW_VERSION = 'sw-v13-39-historique-suppression-confirmation-interne';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
