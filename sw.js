935c806192a0019fac3905b168dc8bdafaa7f668
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-13 09:01:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-32-limite-hdv-maj-bloc-arrivee';
+const SW_VERSION = 'sw-v13-34-bandeau-feu-eta-exports-historique';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
