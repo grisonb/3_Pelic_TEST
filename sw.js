b85b8b4774080fc815fca9b776a32ae039a64af4
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-13 18:05:40 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-40-retour-v13-34-bandeau-feu-suivi-sans-refresh';
+const SW_VERSION = 'sw-v13-41-zoom-ouverture-refracto-historique-exports';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
