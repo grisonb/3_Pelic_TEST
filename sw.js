2d0cd01113605251ee30018776b79d01871684de
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-14 09:57:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-48-pmtiles-repli-zoom-natif-test';
+const SW_VERSION = 'sw-v13-49-pmtiles-repli-zoom-natif-test';
 
 const DB_NAME = 'OfflineTilesDB_v12_21';
 const DB_VERSION = 3;
