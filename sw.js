bed4f173de7dde6e931a18726cde3edff1a2a342
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-22 08:08:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-73_import_cartes_bases_separees';
+const SW_VERSION = 'sw-v13-74_echelle_nm_cartes';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
