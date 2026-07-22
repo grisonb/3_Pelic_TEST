ec984f3e53501071cae2e2e3fec3a71e8b613eb1
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-22 09:29:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-76_regle_echelle_pelic';
+const SW_VERSION = 'sw-v13-77_regle_2doigts_echelle_nm';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
