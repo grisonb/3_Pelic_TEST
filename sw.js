211275fe51e40ec7b3fdaf2fd8568b69968ae0af
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-23 10:26:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-79_bandeaux_icones';
+const SW_VERSION = 'sw-v13-80_pelicandromes_compacts';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
