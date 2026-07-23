a9f0df4f2134836ee18e5c3f4b87c3604731609c
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-23 16:54:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-82_suppression_bouton_maj_commune_droite';
+const SW_VERSION = 'sw-v13-83_bingo_compacts_derout_rotation';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
