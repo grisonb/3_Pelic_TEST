930a618ebe4cebd00e89db7e037654763aa45ebc
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-23 18:02:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-85_affichage_espacements_effectifs';
+const SW_VERSION = 'sw-v13-86_interlignes_colonnes_droite';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
