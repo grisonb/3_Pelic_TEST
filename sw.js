b7285cb13a6ea21dbcef1469e8339cfefbfd022d
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-23 10:39:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'sw-v13-80_pelicandromes_compacts';
+const SW_VERSION = 'sw-v13-81_pelicandromes_reellement_reduits';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
