9ce8823eef8a9403d418cef105a6e6e2a2c59408
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-25 09:55:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v14-04_popup_contact_etiquette_opposee_types_icao';
-const APP_VERSION = 'v14.04';
+const SW_VERSION = 'sw-v14-05_ancrage_leaflet_altitude_opposee_types_affines';
+const APP_VERSION = 'v14.05';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
