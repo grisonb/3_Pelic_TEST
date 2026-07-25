65aa9609f6a31dd34f9684a0e8652acf65f31b56
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-25 19:32:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v14-10_etiquette_sim_sous_avion';
-const APP_VERSION = 'v14.10';
+const SW_VERSION = 'sw-v14-12_blocage_global_stylet';
+const APP_VERSION = 'v14.12';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
