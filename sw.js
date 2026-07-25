a3d21cb36a3325896ca6c3da4740ac8bac6a5d93
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-25 09:38:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v14-03_popup_rapproche_icones_centrees_categories_adsb';
-const APP_VERSION = 'v14.03';
+const SW_VERSION = 'sw-v14-04_popup_contact_etiquette_opposee_types_icao';
+const APP_VERSION = 'v14.04';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
