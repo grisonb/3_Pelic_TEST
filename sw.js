0c372655c9e2d2e87c3f166f685fd87b8b98e752
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-25 10:16:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v14-05_ancrage_leaflet_altitude_opposee_types_affines';
-const APP_VERSION = 'v14.05';
+const SW_VERSION = 'sw-v14-06_etiquette_trafic_identifiant_altitude';
+const APP_VERSION = 'v14.06';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
