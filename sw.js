f8e3fd01389e1bd85c4d5c6fc154fec56bd4e27a
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-25 13:12:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v14-08_deplacement_mode_simulation_vitesse_route';
-const APP_VERSION = 'v14.08';
+const SW_VERSION = 'sw-v14-09_simulation_clavier_altitude_trafic_position';
+const APP_VERSION = 'v14.09';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
