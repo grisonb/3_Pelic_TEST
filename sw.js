73101aa863df126b5f38f8267ee6a37f56229d7e
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-28 21:12:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v14-28_icones_six_categories_alignees_reference_safesky';
-const APP_VERSION = 'v14.28';
+const SW_VERSION = 'sw-v14-29_icones_safesky_base_validee';
+const APP_VERSION = 'v14.29';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
