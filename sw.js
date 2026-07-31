d4470d6e7ffd8c67d5cb8a790d4078243904ae1a
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-31 19:51:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v14-51_croix_aeroport_bandeau_haut';
-const APP_VERSION = 'v14.51';
+const SW_VERSION = 'sw-v14-52_goto_suivi_vert_boutons_dupliques';
+const APP_VERSION = 'v14.52';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
@@ -28,7 +28,7 @@ const APP_SHELL_URLS = [
     './jszip.min.js',
     './communes.json',
     './communes_aliases.json',
-    './data/localites/localites-france-v14.51.zip',
+    './data/localites/localites-france-v14.52.zip',
     HIGH_VOLTAGE_LINES_GEOJSON_URL,
     DEPARTMENTS_GEOJSON_URL,
     './icons/icon-192x192.png',
@@ -53,7 +53,7 @@ const CORE_APP_SHELL_URLS = [
     './jszip.min.js',
     './communes.json',
     './communes_aliases.json',
-    './data/localites/localites-france-v14.51.zip'
+    './data/localites/localites-france-v14.52.zip'
 ];
 
 
@@ -362,7 +362,7 @@ function isAppShellRequest(request) {
             'jszip.min.js',
             'communes.json',
             'communes_aliases.json',
-            'localites-france-v14.51.zip',
+            'localites-france-v14.52.zip',
             'lignes_ht_rte_simplifiees.geojson'
         ].includes(filename) || parsed.pathname.includes('/icons/');
     } catch (_) {
