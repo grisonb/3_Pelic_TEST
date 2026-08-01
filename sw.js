edd4cfb536dfd6556b564129503c63ec0f5109d2
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-01 15:16:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v14-66_restauration_carte_offline_demarrage';
-const APP_VERSION = 'v14.66';
+const SW_VERSION = 'sw-v14-67_restauration_carte_offline_demarrage';
+const APP_VERSION = 'v14.67';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
