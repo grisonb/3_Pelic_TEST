e36c034544cc6198b99adaeff8c1bd2fe54ffb0a
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-08 19:16:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v14-93_vac_offline_github_pages';
-const APP_VERSION = 'v14.93';
+const SW_VERSION = 'sw-v14-94_map_stability_safesky_serialized';
+const APP_VERSION = 'v14.94';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
