80a6e4ebc98ac04ad19f299fc10c14ac837b4c9e
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-10 14:58:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v15-04_maps_button_colors';
-const APP_VERSION = 'v15.04';
+const SW_VERSION = 'sw-v15-05_maps_ui_polish';
+const APP_VERSION = 'v15.05';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
