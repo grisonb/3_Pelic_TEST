c406afd55b3ebd604ef30277f6048f81f879c731
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-17 13:01:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v15-52_sia_suppletive_ctr_popup_vrp';
-const APP_VERSION = 'v15.52';
+const SW_VERSION = 'sw-v15-53_restore_oaci_sia_helper';
+const APP_VERSION = 'v15.53';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
