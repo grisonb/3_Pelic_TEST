21ac5f80e9036266fa85d0bf83dc6b74249d4fda
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-17 13:11:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v15-53_restore_oaci_sia_helper';
-const APP_VERSION = 'v15.53';
+const SW_VERSION = 'sw-v15-54_all_airspaces_ctr_rules';
+const APP_VERSION = 'v15.54';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
