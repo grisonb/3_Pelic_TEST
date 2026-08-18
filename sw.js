0e69e5fa4055163f957fe79852adf608e436821e
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-18 20:32:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const SW_VERSION = 'sw-v15-66_profile_labels_longpress';
-const APP_VERSION = 'v15.66';
+const APP_VERSION = 'v15.67';
 
 const DB_NAME = 'OfflineTilesDB_v13_70_clean';
 const LEGACY_TILE_DB_NAME = DB_NAME;
