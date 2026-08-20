3e0591734433cb325516140d64049c10d7f43931
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-20 15:53:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v15-78_follow_vrp_colors';
-const APP_VERSION = 'v15.78';
+const SW_VERSION = 'sw-v15-79_corrections';
+const APP_VERSION = 'v15.79';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
