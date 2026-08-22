6e997f9ed9b6383dbaa0823376d8b1bff05090ee
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-22 16:37:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v15-85_stable_fire_numbering';
-const APP_VERSION = 'v15.85';
+const SW_VERSION = 'sw-v15-86_traffic_own_glr_pelic_avion_a';
+const APP_VERSION = 'v15.86';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
