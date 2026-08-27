4ab20a792d34a082489e6f3d33197bba2ecc25b1
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-27 14:29:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v15-93_ss_labels_glr_altitude';
-const APP_VERSION = 'v15.93';
+const SW_VERSION = 'sw-v15-94_glr_symbols_filter_ss_labels';
+const APP_VERSION = 'v15.94';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
