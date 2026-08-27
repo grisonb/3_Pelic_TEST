db5361528195cc05385488b9f2610ba9a78e6f39
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-27 10:43:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v15-91_sia_pan_pelic_glr_counts';
-const APP_VERSION = 'v15.91';
+const SW_VERSION = 'sw-v15-92_safesky_glr_national_counts';
+const APP_VERSION = 'v15.92';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
