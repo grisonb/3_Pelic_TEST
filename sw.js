931eb4315c6c00e1ed3594226478a68933f5ef1a
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-29 08:33:18 +0200

    Update APP_VERSION to v16.03

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const SW_VERSION = 'sw-v16-02_perf_ss_tracked_filter';
-const APP_VERSION = 'v16.02';
+const APP_VERSION = 'v16.03';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
