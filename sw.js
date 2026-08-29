3028e76f2b48b8e54ebc6b6edb7d0f817f409006
grisonb <79510016+grisonb@users.noreply.github.com>
2026-08-29 10:09:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-02_perf_ss_tracked_filter';
-const APP_VERSION = 'v16.04';
+const SW_VERSION = 'sw-v16-05_pelic_notams_profile';
+const APP_VERSION = 'v16.05';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
