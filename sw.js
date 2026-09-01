ee435ebef097aa37c846a5c1d64937f34b72c879
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-01 11:04:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-10_waypoint_route_fixes';
-const APP_VERSION = 'v16.10';
+const SW_VERSION = 'sw-v16-11_hyeres_wp_move_insert_labels';
+const APP_VERSION = 'v16.11';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
