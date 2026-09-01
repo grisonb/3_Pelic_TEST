a11375ab16f6f3f54eb520419de93fdf1f58d83d
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-01 18:56:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-22_diag_export_share';
-const APP_VERSION = 'v16.22';
+const SW_VERSION = 'sw-v16-25_wp_source_ring_base_dedup';
+const APP_VERSION = 'v16.25';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
