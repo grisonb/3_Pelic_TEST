5bd8a5c80de41b6cec8f8e10ea99b8478cb1945f
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-02 17:36:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-33_sia_broad_scale_lightweight';
-const APP_VERSION = 'v16.33';
+const SW_VERSION = 'sw-v16-34_sia_zoom_reuse_startup_guard';
+const APP_VERSION = 'v16.34';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
