f09a0663e9ba3588d8d534636bc0d6c35bf2a8e4
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-02 11:40:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-29_pelic_spacing_move_cancel';
-const APP_VERSION = 'v16.29';
+const SW_VERSION = 'sw-v16-30_sia_pan_diagnostic';
+const APP_VERSION = 'v16.30';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
