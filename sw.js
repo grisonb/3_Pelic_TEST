c176c1ac897c4940f30b3e3e057c623c31d27f1e
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-02 09:32:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-28_wp_label_pelic_spacing';
-const APP_VERSION = 'v16.28';
+const SW_VERSION = 'sw-v16-29_pelic_spacing_move_cancel';
+const APP_VERSION = 'v16.29';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
