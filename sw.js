2821e58a49c03d23db193d7ad7bd609e2ac68d56
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-04 15:44:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-48_vac_auto_sync';
-const APP_VERSION = 'v16.48';
+const SW_VERSION = 'sw-v16-49_tiles_queue_ht_canvas';
+const APP_VERSION = 'v16.49';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
