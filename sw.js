02ae00f12f03580e68295c29c47ca3cf92921f7c
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-06 10:55:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-50_vac_scope_diag';
-const APP_VERSION = 'v16.50';
+const SW_VERSION = 'sw-v16-51_diag_gps_layers_search';
+const APP_VERSION = 'v16.51';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
